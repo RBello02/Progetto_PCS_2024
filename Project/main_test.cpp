0bd2b3959b43f23e0afc323819dfbc4ba4a8dac2
unknown <s293527@studenti.polito.it>
2024-06-14 11:54:59 +0200

    risoluzione issues

--- Project/main_test.cpp
+++ Project/main_test.cpp
@@ -1,8 +1,8 @@
 #include <gtest/gtest.h>
-#include "test_Fractures.hpp"
+#include "src_test/test_Fractures.hpp"
 #include "src_test/test_PolygonalMesh.hpp"
 
-int main(int argc, char** argv)
+int main()
 {
     testing::InitGoogleTest();
 
